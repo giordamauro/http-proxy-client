a423c317a086bbc499a2ce853cc9801fa662ba1d
mgiorda <giordamauro@gmail.com>
2014-07-27 15:03:52 -0300

    Added exception logging for client and server exceptions

--- src/main/java/com/http/proxy/impl/ApiResultHandlerImpl.java
+++ src/main/java/com/http/proxy/impl/ApiResultHandlerImpl.java
@@ -11,66 +11,86 @@ import com.http.proxy.ApiResultHandler;
 
 public class ApiResultHandlerImpl implements ApiResultHandler {
 
-    private final Log logger = LogFactory.getLog(this.getClass());
+	private final Log logger = LogFactory.getLog(this.getClass());
 
-    private static final int HTTP_SUCCESS_CODE = 200;
-    private static final int HTTP_REDIRECTION_CODE = 300;
+	private static final int HTTP_SUCCESS_CODE = 200;
+	private static final int HTTP_CLIENT_ERROR_CODE = 400;
+	private static final int HTTP_SERVER_ERRROR_CODE = 500;
 
-    private static final String JSON_CONTENT_TYPE = "application/json";
-    private static final String OCTET_STREAM_CONTENT_TYPE = "application/octet-stream";
+	private static final String JSON_CONTENT_TYPE = "application/json";
+	private static final String OCTET_STREAM_CONTENT_TYPE = "application/octet-stream";
 
-    private final Gson gson;
+	private final Gson gson;
 
-    public ApiResultHandlerImpl(Gson gson) {
-        this.gson = gson;
-    }
+	public ApiResultHandlerImpl(Gson gson) {
+		this.gson = gson;
+	}
 
-    public Object getResponseResult(HttpResponse response, String produces, Type returnType) throws Exception {
+	public Object getResponseResult(HttpResponse response, String produces, Type returnType) throws Exception {
 
-        if (response == null || produces == null || returnType == null) {
-            throw new IllegalArgumentException("Response, produces and returnType cannot be null");
-        }
+		if (response == null || produces == null || returnType == null) {
+			throw new IllegalArgumentException("Response, produces and returnType cannot be null");
+		}
 
-        if (isResponseSuccessful(response.getStatus())) {
+		if (isResponseSuccessful(response.getStatus())) {
 
-            Object result = null;
+			Object result = null;
 
-            if (produces.equals(JSON_CONTENT_TYPE)) {
+			if (produces.equals(JSON_CONTENT_TYPE)) {
 
-                String stringResponse = response.getStringContent();
-                logger.debug(String.format("Parsing json: '%s'", stringResponse));
+				String stringResponse = response.getStringContent();
+				logger.debug(String.format("Parsing json: '%s'", stringResponse));
 
-                result = getFromJSon(returnType, stringResponse);
-            } else if (produces.equals(OCTET_STREAM_CONTENT_TYPE)) {
+				result = getFromJSon(returnType, stringResponse);
+			} else if (produces.equals(OCTET_STREAM_CONTENT_TYPE)) {
 
-                result = response.getInputStream();
-            } else {
-                result = response.getStringContent();
-            }
-            return result;
-        } else {
+				result = response.getInputStream();
+			} else {
+				result = response.getStringContent();
+			}
+			return result;
+		} else {
 
-            if (logger.isDebugEnabled()) {
-                logger.warn(String.format("Response not successful - Code: %s", response.getStatus()));
-            }
+			int responseStatus = response.getStatus();
 
-            Exception exception = getFromJSon(Exception.class, response.getStringContent());
-            throw exception;
-        }
-    }
+			Exception exception = getFromJSon(Exception.class, response.getStringContent());
 
-    private boolean isResponseSuccessful(int responseCode) {
+			if (isClientException(responseStatus)) {
 
-        return responseCode >= HTTP_SUCCESS_CODE && responseCode < HTTP_REDIRECTION_CODE;
-    }
+				if (logger.isDebugEnabled()) {
+					logger.warn(String.format("CLIENT EXCEPTION - Response code: %s", responseStatus), exception);
+				}
+			} else if (isServerException(responseStatus)) {
 
-    private <T> T getFromJSon(Type returnType, String body) {
+				logger.warn(String.format("SERVER EXCEPTION - Response code: %s", responseStatus), exception);
+			}
 
-        try {
-            return gson.fromJson(body, returnType);
-        } catch (Exception e) {
+			throw exception;
+		}
+	}
 
-            throw new IllegalStateException(String.format("Couln't parse json '%s' to class '%s'", body, returnType), e);
-        }
-    }
+	private boolean isResponseSuccessful(int responseCode) {
+
+		return responseCode >= HTTP_SUCCESS_CODE && responseCode < HTTP_SUCCESS_CODE + 100;
+	}
+
+	private boolean isClientException(int responseCode) {
+
+		return responseCode >= HTTP_CLIENT_ERROR_CODE && responseCode < HTTP_CLIENT_ERROR_CODE + 100;
+	}
+
+	private boolean isServerException(int responseCode) {
+
+		return responseCode >= HTTP_SERVER_ERRROR_CODE && responseCode < HTTP_SERVER_ERRROR_CODE + 100;
+	}
+
+	private <T> T getFromJSon(Type returnType, String body) {
+
+		try {
+			return gson.fromJson(body, returnType);
+		} catch (Exception e) {
+
+			throw new IllegalStateException(String.format("Couln't parse json '%s' to class '%s'", body, returnType), e);
+		}
+	}
 }
